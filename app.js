6e6e619eb83ba353e2609d839a062656ec06e1b7
rosariobertona <rosariobertona96@gmail.com>
2021-05-31 01:03:15 -0300

    firt commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const { static } = require("express")
+const express = require ("express")
+const { get } = require("http")
+const app = express ()
+
+const path = require ("path")
+
+app.use (express.static (path.resolve (__dirname , "./public")))
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("servidor corriendo en puerto 3000");
+})
+
+
+app.get ("/", (req, res)=> res.sendFile(path.resolve (__dirname , "views", "home.html")))
+app.get ("/register", (req, res)=> res.sendFile(path.resolve (__dirname , "views", "register.html")))
+app.get ("/login", (req, res)=> res.sendFile(path.resolve (__dirname , "views", "login.html")))
+
+
+
